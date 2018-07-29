7b472acb10c059a0177319e2c3418d27c8f78bc6
Bernard Cooley <bernardcooley@gmail.com>
2018-07-29 20:35:56 +0100

    music player added

--- src/components/Music.vue
+++ src/components/Music.vue
@@ -7,19 +7,84 @@
         </ion-header>
 
         <ion-content class="content">
-            <audio controls>
-                <source src="../assets/music/tape_twelve_slice.mp3" type="audio/mpeg">
-                Your browser does not support the audio element.
-            </audio>
+            <ion-card class="ionCard">
+                <ion-card-header>
+                    Tracks
+                </ion-card-header>
+
+                <ion-list v-for="(track, index) of musicFiles">
+                    <div class="trackContainer">
+                        <h4>{{track.artist}} - {{track.trackName}}</h4>
+                        <audio controls class="track">
+                            <source v-bind:src='track.url' type="audio/mpeg"> 
+                            Your browser does not support the audio element.
+                        </audio>
+                        <ion-button>Save</ion-button>
+                        <ion-button>More info</ion-button>
+                    </div>
+                </ion-list>
+            </ion-card>
         </ion-content>
     </ion-page>
 </template>
 
 <script>
 
+export default {
+    data() {
+        return {
+            musicFiles: [
+                {
+                    "artist": "Tape Twelve",
+                    "trackName": "Slice",
+                    "url": "../assets/music/tape_twelve_slice.mp3"
+                },
+                {
+                    "artist": "Tape Twelve",
+                    "trackName": "Temper",
+                    "url": "../assets/music/tape_twelve_temper.mp3"
+                },
+                {
+                    "artist": "Tape Twelve",
+                    "trackName": "Temper",
+                    "url": "../assets/music/tape_twelve_temper.mp3"
+                },
+                {
+                    "artist": "Tape Twelve",
+                    "trackName": "Temper",
+                    "url": "../assets/music/tape_twelve_temper.mp3"
+                },
+                {
+                    "artist": "Tape Twelve",
+                    "trackName": "Temper",
+                    "url": "../assets/music/tape_twelve_temper.mp3"
+                },
+                {
+                    "artist": "Tape Twelve",
+                    "trackName": "Temper",
+                    "url": "../assets/music/tape_twelve_temper.mp3"
+                },
+                {
+                    "artist": "Tape Twelve",
+                    "trackName": "Temper",
+                    "url": "../assets/music/tape_twelve_temper.mp3"
+                }
+            ]
+        }
+    }
+}
 
 </script>
 
 <style>
 
+.track {
+    margin: auto;
+}
+
+.trackContainer {
+    margin: 5px;
+    padding: 5px;
+    background-color: rgba(0, 0, 0, 0.1);
+}
 </style>
