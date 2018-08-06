c4600f034e5d88f6386f8a04e6519e595b2ebe00
Bernard Cooley <bernardcooley@gmail.com>
2018-08-06 17:08:22 +0100

    music player styled

--- src/components/AudioPlayer.vue
+++ src/components/AudioPlayer.vue
@@ -1,49 +1,56 @@
 <template>
 	<div>
-		<h4>Artist: {{artist}}</h4>
-		<h4>Title: {{trackName}}</h4>
 		<div class="player">
-			<div>
-				<div v-on:click="seek" class="player-progress" title="Time played : Total time">
-					<div :style="{ width: this.percentComplete + '%' }" class="player-seeker"></div>
-				</div>
-				<div class="player-time">
-					<div class="player-time-current">{{ currentTime }}</div>
-					<div class="player-time-total">{{ durationTime }}</div>
-				</div>
-			</div>
+
 			<div class="player-controls">
 				<div>
-					<a v-on:click.prevent="stop" title="Stop" href="#">
-						<img src="../assets/icons/stop_button.svg">
-					</a>
+					<div v-on:click="seek" class="player-progress" title="Time played : Total time">
+						<div :style="{ width: this.percentComplete + '%' }" class="player-seeker"></div>
+					</div>
+					<div class="player-time">
+						<div class="player-time-current">{{ currentTime }}</div>
+						<div class="player-time-total">{{ durationTime }}</div>
+					</div>
+					<div class="artworkContainer">
+						<img v-bind:src="artworkUrl">
+					</div>
 				</div>
 				<div>
-					<a v-on:click.prevent="playing = !playing" title="Play/Pause" href="#">
-						<img v-if="!playing" src="../assets/icons/play_button.svg">
-						<img v-else src="../assets/icons/pause_button.svg">
-					</a>
-				</div>
+					<div>
+						<a v-on:click.prevent="stop" title="Stop" href="#">
+							<img src="../assets/icons/stop_button.svg">
+						</a>
+					</div>
+					<div>
+						<a v-on:click.prevent="playing = !playing" title="Play/Pause" href="#">
+							<img v-if="!playing" src="../assets/icons/play_button.svg">
+							<img v-else src="../assets/icons/pause_button.svg">
+						</a>
+					</div>
 
-				<div>
-					<a v-on:click.prevent="download" href="#">
-						<img src="../assets/icons/download_button.svg">
-					</a>
-				</div>
-				<div>
-					<a v-on:click.prevent="innerLoop = !innerLoop" href="#">
-						<img v-if="!innerLoop" src="../assets/icons/start_loop_button.svg">
-						<img v-else src="../assets/icons/stop_loop_button.svg">
-					</a>
-				</div>
-				<div>
-					<a v-on:click.prevent="mute" title="Mute" href="#">
-						<img v-if="!muted" src="../assets/icons/mute_button.svg">
-						<img v-else src="../assets/icons/unmute_button.svg">
-					</a>
+					<div>
+						<a v-on:click.prevent="download" href="#">
+							<img src="../assets/icons/download_button.svg">
+						</a>
+					</div>
+					<div>
+						<a v-on:click.prevent="innerLoop = !innerLoop" href="#">
+							<img v-if="!innerLoop" src="../assets/icons/start_loop_button.svg">
+							<img v-else src="../assets/icons/stop_loop_button.svg">
+						</a>
+					</div>
+					<div>
+						<a v-on:click.prevent="mute" title="Mute" href="#">
+							<img v-if="!muted" src="../assets/icons/mute_button.svg">
+							<img v-else src="../assets/icons/unmute_button.svg">
+						</a>
+					</div>
 				</div>
 			</div>
-			<input class="volumeControl" v-model.lazy.number="volume" type="range" min="0" max="100" />
+			<div class="volumeContainer">
+				<span>Vol</span>
+				<input class="volumeControl slider" v-model.lazy.number="volume" type="range" min="0" max="100" />
+			</div>
 			<audio :loop="innerLoop" ref="audiofile" :src="file" preload="auto" style="display: none;"></audio>
 		</div>
 	</div>
@@ -77,6 +84,10 @@ export default {
 			default: null
 		},
 		trackName: {
+			type: String,
+			default: null
+		},
+		artworkUrl: {
 			type: String,
 			default: null
 		}
@@ -180,7 +191,7 @@ body {
 	text-rendering: optimizeLegibility;
 }
 
-$player-background: #fff;
+$player-background: #F4F4F4;
 $player-border-color: darken($player-background,
 12%);
 $player-link-color: darken($player-background,
@@ -191,14 +202,105 @@ $player-text-color: $player-link-color;
 
 .player-wrapper {
 	background-color: $player-background;
-	background-image: linear-gradient(90deg, #fff 0, darken(#fff, 12%));
 	display: flex;
 	justify-content: center;
 	padding-bottom: 20px;
 }
 
+input[type=range].slider {
+  -webkit-appearance: none;
+  width: 100%;
+  margin: 0.9px 0;
+}
+input[type=range].slider:focus {
+  outline: none;
+}
+input[type=range].slider::-webkit-slider-runnable-track {
+  width: 100%;
+  height: 9.2px;
+  cursor: pointer;
+  box-shadow: 0px 0px 0.6px rgba(0, 0, 0, 0.41), 0px 0px 0px rgba(13, 13, 13, 0.41);
+  background: rgba(0, 0, 0, 0);
+  border-radius: 0px;
+  border: 0px solid rgba(0, 0, 0, 0);
+}
+input[type=range].slider::-webkit-slider-thumb {
+  box-shadow: 15px 15px 15px #ffffff, 0px 0px 15px #ffffff;
+  border: 0px solid #ffffff;
+  height: 11px;
+  width: 24px;
+  border-radius: 34px;
+  background: rgba(0, 0, 0, 0.97);
+  cursor: pointer;
+  -webkit-appearance: none;
+  margin-top: -0.9px;
+}
+input[type=range].slider:focus::-webkit-slider-runnable-track {
+  background: rgba(13, 13, 13, 0);
+}
+input[type=range].slider::-moz-range-track {
+  width: 100%;
+  height: 9.2px;
+  cursor: pointer;
+  box-shadow: 0px 0px 0.6px rgba(0, 0, 0, 0.41), 0px 0px 0px rgba(13, 13, 13, 0.41);
+  background: rgba(0, 0, 0, 0);
+  border-radius: 0px;
+  border: 0px solid rgba(0, 0, 0, 0);
+}
+input[type=range].slider::-moz-range-thumb {
+  box-shadow: 15px 15px 15px #ffffff, 0px 0px 15px #ffffff;
+  border: 0px solid #ffffff;
+  height: 11px;
+  width: 24px;
+  border-radius: 34px;
+  background: rgba(0, 0, 0, 0.97);
+  cursor: pointer;
+}
+input[type=range].slider::-ms-track {
+  width: 100%;
+  height: 9.2px;
+  cursor: pointer;
+  background: transparent;
+  border-color: transparent;
+  color: transparent;
+}
+input[type=range].slider::-ms-fill-lower {
+  background: rgba(0, 0, 0, 0);
+  border: 0px solid rgba(0, 0, 0, 0);
+  border-radius: 0px;
+  box-shadow: 0px 0px 0.6px rgba(0, 0, 0, 0.41), 0px 0px 0px rgba(13, 13, 13, 0.41);
+}
+input[type=range].slider::-ms-fill-upper {
+  background: rgba(0, 0, 0, 0);
+  border: 0px solid rgba(0, 0, 0, 0);
+  border-radius: 0px;
+  box-shadow: 0px 0px 0.6px rgba(0, 0, 0, 0.41), 0px 0px 0px rgba(13, 13, 13, 0.41);
+}
+input[type=range].slider::-ms-thumb {
+  box-shadow: 15px 15px 15px #ffffff, 0px 0px 15px #ffffff;
+  border: 0px solid #ffffff;
+  height: 11px;
+  width: 24px;
+  border-radius: 34px;
+  background: rgba(0, 0, 0, 0.97);
+  cursor: pointer;
+  height: 9.2px;
+}
+input[type=range].slider:focus::-ms-fill-lower {
+  background: rgba(0, 0, 0, 0);
+}
+input[type=range].slider:focus::-ms-fill-upper {
+  background: rgba(13, 13, 13, 0);
+}
+
+
+
+.artworkContainer {
+	width: 200px;
+}
+
 .player {
-	background-color: $player-background;
+	background-color: white;
 	border: 1px solid $player-border-color;
 	border-radius: 5px;
 	box-shadow: 0 5px 8px rgba(0, 0, 0, 0.15);
@@ -209,6 +311,13 @@ $player-text-color: $player-link-color;
 
 	.volumeControl {
 		width: 100%;
+	}
+	.volumeContainer {
+		display: flex;
+
+		span {
+			padding-right: 10px;
+		}
 	}
 }
 
@@ -235,7 +344,7 @@ $player-text-color: $player-link-color;
 .player-progress {
 	background-color: $player-progress-color;
 	cursor: pointer;
-	height: 50%;
+	height: 20px;
 	min-width: 200px;
 	position: relative;
 
--- src/components/Music.vue
+++ src/components/Music.vue
@@ -2,23 +2,26 @@
     <ion-page>
         <ion-header>
             <ion-toolbar class="toolbar-md-primary">
-                <ion-title>Music</ion-title>
+                <ion-title>Shared tracks</ion-title>
             </ion-toolbar>
         </ion-header>
 
         <ion-content class="content">
-            <ion-card class="ionCard">
-                <ion-card-header>
-                    Tracks
-                </ion-card-header>
 
-                <ion-list v-for="(track, index) in Tracks" v-bind:data="track" v-bind:key="track.index">
+            <!--<ion-card-header><h2>Shared tracks</h2></ion-card-header>-->
 
+            <ion-list v-for="(track, index) in Tracks" v-bind:data="track" v-bind:key="track.index">
+                <ion-card class="ionCard">
+                    <ion-card-header>
+                        <h4>Artist: {{track.artist}}</h4>
+                        <h4>Title: {{track.trackName}}</h4>
+                    </ion-card-header>
                     <div id="audio" class="player-wrapper">
-                        <audio-player v-bind:file='track.url' v-bind:artist='track.artist' v-bind:trackName='track.trackName'></audio-player>
+                        <audio-player v-bind:file='track.url' v-bind:artist='track.artist' v-bind:trackName='track.trackName' v-bind:artworkUrl='track.artworkUrl'></audio-player>
                     </div>
-                </ion-list>
-            </ion-card>
+                </ion-card>
+            </ion-list>
+
         </ion-content>
     </ion-page>
 </template>
@@ -42,7 +45,6 @@ export default {
 </script>
 
 <style>
-
 .track {
     margin: auto;
 }
@@ -51,5 +53,13 @@ export default {
     margin: 5px;
     padding: 5px;
     background-color: rgba(0, 0, 0, 0.1);
+}
+
+.content {
+    background-color: white;
+}
+
+.card-header {
+    background-color: #F4F4F4;
 }
 </style>
